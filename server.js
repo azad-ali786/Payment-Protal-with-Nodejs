cf99ba7248a09ba7b1ae536d846b87f8aaed76de
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-04-23 00:15:24 +0530

    Rendered Json

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-const express = require('express')
-const app = express()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/store", function (req, res) {
+  fs.readFile('item.json', function (err,data) {
+    if (err) {
+      res.status(500).end();
+    }else{
+      res.render('store.ejs',{
+        items: JSON.parse(data),
+      })
+    }
+  })
+});
+
+app.listen(3000);
